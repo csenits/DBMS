ee3ddf69365152119b4037d5f8f1f2a0b6218b8c
Abdullah Khilji <abdullahkhilji.nits@gmail.com>
2020-02-18 16:32:07 +0530

    5 completed

--- 5.sql
+++ 5.sql
@@ -1,6 +1,13 @@
 1.
-SELECT onum,
-       snum,
-       amt,
-       (amt*12)/100 commision
-FROM orders;
+SELECT Onum, Snum, Amt, (Amt * 12) / 100 Commission
+FROM ORDERS;
+
+
+2.
+SELECT City, max(Rating) FROM CUSTOMERS GROUP BY City;
+
+3.
+SELECT Rating, Cname, Snum FROM CUSTOMERS ORDER BY Rating DESC;
+
+4.
+SELECT count(Odate) totalOrders, Odate FROM ORDERS GROUP BY Odate ORDER BY totalOrders;
